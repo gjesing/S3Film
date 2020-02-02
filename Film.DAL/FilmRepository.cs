5a74c4258d17b5600e271e56c2807075b462d7d6
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-02-02 23:12:28 +0100

    Methods in FilmRepository improved and method UpdateFilm(Film film) created

--- Film.DAL/FilmRepository.cs
+++ Film.DAL/FilmRepository.cs
@@ -14,59 +14,35 @@ namespace S3Film.DAL
         /// Retrieves all Films from the database.
         /// </summary>
         /// <returns>A List of all Films</returns>
-        public List<Film> GetFilms()
-        {
-            string sql = "SELECT * FROM Film ORDER BY Titel";
-            return HandleData(ExecuteQuery(sql));
-        }
-
+        public List<Film> GetFilms() => HandleData(ExecuteQuery("SELECT * FROM Film ORDER BY Titel"));
+        
         /// <summary>
         /// Gets all Films from the database where Titel contains the specified string.
         /// </summary>
         /// <param name="titel">The string to search for in the Film Titels</param>
         /// <returns>A list of Films containing the specified string</returns>
-        public List<Film> GetFilms(string titel)
-        {
-            string sql = $"SELECT * FROM Film WHERE Titel LIKE '%{titel}%'";
-            return HandleData(ExecuteQuery(sql));
-        }
+        public List<Film> GetFilms(string titel) => HandleData(ExecuteQuery($"SELECT * FROM Film WHERE Titel LIKE '%{titel}%'"));
 
         /// <summary>
         /// Inserts Film into database.
         /// </summary>
         /// <param name="film">The Film to be inserted into the database</param>
-        /// <returns>A string telling the validation result</returns>
-        public string InsertFilm(Film film)
-        {
-            bool isValid = IsValid(film, out string message);
-            if (!isValid)
-            {
-                return message;
-            }
-            string sql = $"INSERT INTO Film VALUES ('{film.Titel}', '{film.Land}', {film.Year}, '{film.Genre}', {film.Oscars})";
-            int affectedRows = ExecuteNonQuery(sql);
-            if (affectedRows > 0)
-            {
-                return message;
-            }
-            message = "Filmen kunne ikke oprettes.";
-            return message;
-        }
+        /// <returns>A string returning the validation result</returns>
+        public string InsertFilm(Film film) => IsValid(film) ? (ExecuteNonQuery($"INSERT INTO Film VALUES ('{film.Titel}', '{film.Land}', {film.Year}, '{film.Genre}', {film.Oscars})") > 0 ? $"Filmen \"{film.Titel}\" er oprettet." : "Alle felter skal udfyldes.") : "Filmen kunne ikke oprettes.";
 
-        public string DeleteFilm(Film film)
-        {
-            string message;
-            string sql = $"DELETE FROM Film WHERE FilmId = {film.Id}";
-            if (ExecuteNonQuery(sql) > 0)
-            {
-                message = "Filmen er slettet.";
-            }
-            else
-            {
-                message = "Filmen kunne ikke slettes";
-            }
-            return message;
-        }
+        /// <summary>
+        /// Updates Film in database.
+        /// </summary>
+        /// <param name="film">The film to be updated in the database</param>
+        /// <returns>A string returning the validation result</returns>
+        public string UpdateFilm(Film film) => IsValid(film) ? (ExecuteNonQuery($"UPDATE Films SET Titel = ('{film.Titel}', Land = '{film.Land}', Year = {film.Year}, Oscars = {film.Oscars} WHERE FilmId = {film.Id}") > 0 ? $"Filmen \"{film.Titel}\" (Id: {film.Id}) er opdateret." : "Alle felter skal udfyldes.") : "Filmen kunne ikke opdateres.";
+
+        /// <summary>
+        /// Deletes Film from database
+        /// </summary>
+        /// <param name="film">The Film to be deleted from the database</param>
+        /// <returns>A string returning the result</returns>
+        public string DeleteFilm(Film film) => (ExecuteNonQuery($"DELETE FROM Film WHERE FilmId = {film.Id}") > 0) ? "Filmen er slettet." : "Filmen kunne ikke slettes";
 
         /// <summary>
         /// Helper method used to convert DataTable to list of Films.
@@ -76,22 +52,8 @@ namespace S3Film.DAL
         private List<Film> HandleData(DataTable dataTable)
         {
             List<Film> films = new List<Film>();
-            if (dataTable is null)
-                return films;
-
-            foreach (DataRow row in dataTable.Rows)
-            {
-                Film film = new Film()
-                {
-                    Id = (int)row["FilmId"],
-                    Titel = (string)row["Titel"],
-                    Land = (string)row["Land"],
-                    Year = (int)row["Year"],
-                    Genre = (string)row["Genre"],
-                    Oscars = (int)row["Oscars"]
-                };
-                films.Add(film);
-            }
+            if (dataTable is null) return films;
+            dataTable.Rows.Cast<DataRow>().ToList().ForEach(row => { films.Add(new Film(){ Id = (int)row["FilmId"], Titel = (string)row["Titel"], Land = (string)row["Land"], Year = (int)row["Year"], Genre = (string)row["Genre"], Oscars = (int)row["Oscars"] }); });
             return films;
         }
 
@@ -101,15 +63,7 @@ namespace S3Film.DAL
         /// <param name="film">The Film to be validated</param>
         /// <param name="message">A string telling the result of the validation</param>
         /// <returns>false if any of the properties of the Film is null, else true</returns>
-        private bool IsValid(Film film, out string message)
-        {
-            if (string.IsNullOrWhiteSpace(film.Titel) || string.IsNullOrWhiteSpace(film.Land) || string.IsNullOrWhiteSpace(film.Genre))
-            {
-                message = "Alle felter skal udfyldes.";
-                return false;
-            }
-            message = $"Filmen {film.Titel} er oprettet.";
-            return true;
-        }
+        private bool IsValid(Film film) => (string.IsNullOrWhiteSpace(film.Titel) || string.IsNullOrWhiteSpace(film.Land) || string.IsNullOrWhiteSpace(film.Genre)) ? false : true;
+         
     }
 }
