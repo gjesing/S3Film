5a5cebccb1384327c90cfd83fa483ad3f38abe24
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-02-03 14:59:55 +0100

    CreateFilm UserControl created and used in MainWindow

---
+++ S3Film.GUI/UserControls/CreateFilm.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S3Film.GUI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CreateFilm.xaml
+    /// </summary>
+    public partial class CreateFilm : UserControl
+    {
+        public CreateFilm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateFilm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
